bb3c0ac3988b6ecab79f31a42f40c5d34ebe3973
ReeyaB123 <reeya4151@gmail.com>
2022-02-06 03:14:34 +0000

    commit3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,5 @@
 <script>
-    import Greeting from '$lib/Greeting.svelte';
-    import Entry from '$lib/Entry.svelte';
-    import EntryModal from '$lib/EntryModal.svelte';
+    
     import supabase from '$lib/db';
 
     async function signOut() {
@@ -19,44 +17,14 @@
     }
 </script>
 
-<Greeting />
-<!--   Entries   -->
-<section class="container px-4 py-3">
-    <div class="d-flex justify-content-between">
-   	 <div class="p-2">Mood Log</div>
-   	 <input
-   		 class="btn btn-light mb-2"
-   		 type="button"
-   		 value="+ New Entry"
-   		 data-bs-toggle="modal"
-   		 data-bs-target="#newEntry"
-   	 />
-    </div>
 
-    <div class="list-group mb-3">
-   	 <!-- Individual Entries -->
-   	 {#await getEntries()}
-   		 <p>Fetching data...</p>
-   	 {:then data}
-   		 {#each data as entry}
-   			 <Entry
-   				 date={entry.day + '-' + entry.month + '-' + entry.year}
-   				 mood={entry.mood}
-   				 comment={entry.comment}
-   			 />
-   		 {/each}
-   	 {:catch error}
-   		 <p>Something went wrong while fetching the data:</p>
-   		 <pre>{error}</pre>
-   	 {/await}
-    </div>
-</section>
+
 
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
     <button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
 
-<EntryModal />
+
 
    
