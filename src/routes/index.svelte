5f0e00542d0cd11f432cc8528e18bd0c423c2650
ReeyaB123 <reeya4151@gmail.com>
2022-02-18 10:31:07 +0000

    commit6

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,361 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import supabase from '$lib/db';
+
+    async function signOut() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+    let timetable = {
+	Monday: [
+  	{
+    	name: "PH",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BI",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "R",
+    	period: 1,
+    	style: "table-primary",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Tuesday: [
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BI",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "E",
+    	period: 1,
+    	style: "table-primary",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+	],
+	Wednesday: [
+  	{
+    	name: "BC",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "H",
+    	period: 1,
+    	style: "table-primary",
+  	},
+  	{
+    	name: "PKS",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Thursday: [
+  	{
+    	name: "SA",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "A",
+    	period: 1,
+    	style: "table-primary",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Friday: [
+  	{
+    	name: "BI",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "T",
+    	period: 1,
+    	style: "table-primary",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "SA",
+    	period: 2,
+    	style: "",
+  	},
+	],
+  };
+function addTimeSlot(day){
+    if(day == "Monday"){
+        timetable.Monday = [
+            ...timetable.Monday,
+    { name: "??", period: 1, style: "" }
+ ];
+       
+    }
+
+else if(day == "Tuesday"){
+    timetable.Tuesday = [
+            ...timetable.Tuesday,
+    { name: "??", period: 1, style: "" }
+ ];
+}
+else if(day == "Wednesday"){
+    timetable.Wednesday = [
+            ...timetable.Wednesday,
+    { name: "??", period: 1, style: "" }
+ ];
+}
+else if(day == "Thursday"){
+    timetable.Thursday = [
+            ...timetable.Thursday,
+    { name: "??", period: 1, style: "" }
+ ];
+}
+else{
+    timetable.Friday = [
+            ...timetable.Friday,
+    { name: "??", period: 1, style: "" }
+ ];
+}
+}
+let curDay;
+let curIndex;
+let curName;
+let curPeriod;
+let curStyle;
+
+function showCurData(day,index,name,period,style){
+    curDay = day;
+    curIndex = index;
+    curName = name;
+    curPeriod = period;
+    curStyle = style;
+
+}
+</script>    
+
+
+<table class="table table-striped table-bordered text-center">
+  <thead>
+    <tr class="table-info">
+      <th scope="col">#</th>
+      <th scope="col">1</th>
+      <th scope="col">2</th>
+      <th scope="col">3</th>
+      <th scope="col">4</th>
+      <th scope="col">-</th>
+      <th scope="col">5</th>
+      <th scope="col">6</th>
+      <th scope="col">7</th>
+      <th scope="col">8</th>
+      <th scope="col">9</th>
+      <th scope="col">10</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">MON</th>
+     {#each timetable.Monday as timeSlot, index}
+      	<td colspan={timeSlot.period} class={timeSlot.style}>
+          <button class="btn" type="button"data-bs-toggle="modal" data-bs-target = "#editTimeSlot" on:click = {() => showCurData("Monday",index,timeSlot.name, timeSlot.period, timeSlot.style)}>{timeSlot.name}</button>
+          
+        </td>
+     {/each}
+     <td>
+         <button class = "btn" on:click={() => addTimeSlot("Monday")}>+</button>
+     </td>
+    </tr>
+    <tr>
+      <th scope="row">TUE</th>
+      {#each timetable.Tuesday as timeSlot, index}
+      <td colspan={timeSlot.period} class={timeSlot.style}>
+      <button class="btn" type="button"data-bs-toggle="modal" data-bs-target = "#editTimeSlot">{timeSlot.name}</button>
+     
+    </td>
+ {/each}
+ <td>
+    <button class = "btn" on:click={() => addTimeSlot("Tuesday")}>+</button>
+</td>
+    
+    </tr>
+    <tr>
+      <th scope="row">WED</th>
+      {#each timetable.Wednesday as timeSlot, index}
+      <td colspan={timeSlot.period} class={timeSlot.style}>
+      <button class="btn" type="button"data-bs-toggle="modal" data-bs-target= "#editTimeSlot">{timeSlot.name}</button>
+
+    </td>
+ {/each}
+ <td>
+    <button class = "btn" on:click={() => addTimeSlot("Wednesday")}>+</button>
+</td>
+    </tr>
+    <tr>
+      <th scope="row">THU</th>
+      {#each timetable.Thursday as timeSlot, index}
+      <td colspan={timeSlot.period} class={timeSlot.style}>
+      <button class="btn" type="button"data-bs-toggle="modal" data-bs-target= "#editTimeSlot">{timeSlot.name}</button>
+
+    </td>
+ {/each}
+ <td>
+    <button class = "btn" on:click={() => addTimeSlot("Thursday")}>+</button>
+</td>
+    </tr>
+    <tr>
+      <th scope="row">FRI</th>
+      {#each timetable.Friday as timeSlot, index}
+      <td colspan={timeSlot.period} class={timeSlot.style}>
+      <button class="btn" type="button"data-bs-toggle="modal" data-bs-target= "#editTimeSlot">{timeSlot.name}</button>
+
+    </td>
+ {/each} <td>
+    <button class = "btn" on:click={() => addTimeSlot("Friday")}>+</button>
+</td>
+
+    </tr>
+  </tbody>
+</table>
+
+ <!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={signOut}>Logout</button>
+</section>
+
+<div class="modal fade" id="editTimeSlot" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Edit Time Slot</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <form>
+            <div class="input-group mb-3 ">
+                <span class="input-group-text w-25" id="addon-wrapping">Name</span>
+                <input type="text" class="form-control" placeholder="Subject" aria-label="Subject" aria-describedby="addon-wrapping" bind:value = {curName}>
+              </div>
+              <div class="input-group mb-3">
+                <span class="input-group-text w-25" id="addon-wrapping">Period</span>
+                <input type="number" class="form-control" placeholder="Time Slot" aria-label="Time Slot" aria-describedby="addon-wrapping" bind:value = {curPeriod}>
+              </div>
+              <div class="input-group mb-3">
+                <label class="input-group-text w-25" for="inputGroupSelect01">Style</label>
+                <select class="form-select" id="inputGroupSelect01" bind:value = {curStyle}>
+                  <option value = "">Default</option>
+                  <option value="table-primary">Blue</option>
+                  <option value="table-success">Green</option>
+                  <option value="table-danger">Red</option>
+                  <option value="table-warning">Yellow</option>
+                  <option value="table-secondary">Grey</option>
+                </select>
+              </div>
+        </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger">Delete</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
